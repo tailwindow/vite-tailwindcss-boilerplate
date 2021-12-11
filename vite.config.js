1389c23ed39cc9ba95a354890dda853563fecfde
Martien Dermawan Tanama <martiendt@gmail.com>
2021-12-11 08:10:17 +0700

    bump

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import mkcert from'vite-plugin-mkcert'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), mkcert()],
   server: {
     port: 8080,
-    https: true
+    https: true,
   },
   resolve: {
     alias: [
       {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src')
-      }
-    ]
-  }
-})
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+});
